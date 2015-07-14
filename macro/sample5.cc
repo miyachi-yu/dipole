0f07e9bc5cb66a2575635b2aaf40c84fe186e2e3
miyachi-yu <miyachi@sci.kj.yamagata-u.ac.jp>
2015-07-14 18:52:10 +0859

    small fix

--- macro/sample5.cc
+++ macro/sample5.cc
@@ -14,15 +14,23 @@ int sample5(){
   // ESR spectrum of TEMPO doped material
   ESR esr( "cal2_1.txt", 64 ); // prepare ESR data from the given file
 
-  // ESR lines are at 319.8, 321.3 and 322.8
-  app->toffset( 319.8 );
-  app->toffset( 321.3 );
-  app->toffset( 322.8 );
+  // Tunning of numerical integration parameteres.
+  app->precision( 0.0001 );
+  app->nGrid( 10 );
+  app->nLeg( 7, 8 );
   
-  app->amplitude( 150.0 );
-  app->mean( 0.65 );
-  app->sigma( 0.2 );
-  app->asym( 3.0 );
+  // ESR lines are at 319.8, 321.3 and 322.8
+  // by repeating toffset methods, the new esr line positions are added.
+  // (not replacing the stored value)
+  app->toffset( 319.80 );
+  app->toffset( 321.3 );
+  app->toffset( 322.84 );
+
+  // configure density distribution
+  app->amplitude( 90.0 );
+  app->mean( 1.025 );
+  app->sigma( 0.0425 );
+  app->asym( 9.0 );
 
   TCanvas *c = new TCanvas( "c1", "ESR", 794, 1123 );
   c->Divide( 1, 3 );
@@ -31,7 +39,7 @@ int sample5(){
 
   c->cd( 2 );
   app->drawI( &esr );
-  app->drawI( 316, 326.6 );
+  app->drawI( 317.0, 325.0 );
 
   c->cd( 3 );
   esr.GetGraph( true )->DrawClone( "Al" );
