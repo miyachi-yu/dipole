fb6d79fc031362b1c42bee056b65a6f43ca123fd
miyachi-yu <miyachi@sci.kj.yamagata-u.ac.jp>
2015-07-15 12:36:05 +0859

    modified this example to perform fit to the TEMPO spectrum.

--- macro/sample5.cc
+++ macro/sample5.cc
@@ -1,11 +1,8 @@
 /* ----------------------------------------------------------------
    file:         sample5.cc
    description: 
-   Example for ESR data handling. Details of the ESR class can be
-   found in Quark Group Wiki Page
-   http://www.quark.kj.yamagata-u.ac.jp/dokuwiki/doku.php?id=quark:pt:esr
-
-   esr2.zip was imported to this analsys software.
+   Draw 3 ESR lines like TEMPO with Dipole-dipole interaction. 
+   As sample3.cc, ESR data will be fitted LineShape wrapper class.
    ---------------------------------------------------------------- */
 int sample5(){
 
@@ -22,31 +19,52 @@ int sample5(){
   // ESR lines are at 319.8, 321.3 and 322.8
   // by repeating toffset methods, the new esr line positions are added.
   // (not replacing the stored value)
-  app->toffset( 319.80 );
-  app->toffset( 321.3 );
+  app->toffset( 319.75 );
+  app->toffset( 321.35 );
   app->toffset( 322.84 );
 
   // configure density distribution
-  app->amplitude( 90.0 );
-  app->mean( 1.025 );
-  app->sigma( 0.0425 );
-  app->asym( 9.0 );
+  app->amplitude( 46.66 );
+  app->mean( 0.8989 );
+  app->sigma( 0.0501 );
+  app->asym( 11.28 );
 
+  // get wrapper class object for TF1
+  double sig[2] = { 318.0, 325.0 };
+  LineShape* lS = app->lineShapeObj();
+  TF1 *f1 = new TF1( "lineShape", lS, sig[0], sig[1], 5, "LineShape" );
+  // set initial values
+  f1->SetParameter( 0, app->amplitude() );
+  f1->SetParameter( 1, app->mean() );
+  f1->SetParameter( 2, app->sigma() );
+  f1->SetParameter( 3, app->asym()  );
+  f1->FixParameter( 4, 0.0   );            // offset constant
+
+  // style setting
+  f1->SetLineColor( kMagenta );
+  f1->SetLineStyle( 2 );
+  f1->SetLineWidth( 2 );
+  
   TCanvas *c = new TCanvas( "c1", "ESR", 794, 1123 );
   c->Divide( 1, 3 );
   c->cd( 1 );
   app->drawRho();
 
-  c->cd( 2 );
-  app->drawI( &esr );
-  app->drawI( 317.0, 325.0 );
-
   c->cd( 3 );
   esr.GetGraph( true )->DrawClone( "Al" );
 
-  
-  // draw the density and intensity distributions.
-  // The given esr data will be super imposed on the intensity distribution
+  c->cd( 2 );
+  app->drawI( &esr );
+
+  c->Update();
+  //
+  //  Data preparation
+  //
+  TGraph* g = (TGraph*) esr.GetGraphInteg()->Clone();
+  g->Fit( f1, "VNME", "", sig[0], sig[1] );
+
+  c->cd( 2 );
+  app->drawI( 314.0, 329.0 );
   
   return 0;
 }
